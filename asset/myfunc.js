9f9de8680c24277b62b9e9df385563aa73a8f9b6
Pavneet Jauhal <pavneetjauhal@Pavneets-MacBook-Pro.local>
2025-12-30 22:30:38 -0500

    add full recoomendations

--- asset/myfunc.js
+++ asset/myfunc.js
@@ -1,11 +1,6 @@
 /* global CustomFunctions */
 
-// Load the Custom Functions runtime (required)
-importScripts(
-  "https://appsforoffice.microsoft.com/lib/1/custom-functions-runtime.js"
-);
-
-function getrandom(dataType) {
+function getRandomData(dataType) {
   if (dataType === "phone") {
     return "5432167890";
   }
@@ -15,5 +10,4 @@ function getrandom(dataType) {
   return "not supported";
 }
 
-// Register the function
-CustomFunctions.associate("RANDOM", getrandom);
+CustomFunctions.associate("GETRANDOMDATA", getRandomData);
