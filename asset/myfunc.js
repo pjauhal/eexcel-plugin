cf0bc514ea58caaca9a6b0398c6963cbe79ea021
Pavneet Jauhal <pavneetjauhal@Pavneets-MacBook-Pro.local>
2025-12-30 21:51:23 -0500

    use worker

--- asset/myfunc.js
+++ asset/myfunc.js
@@ -1,12 +1,17 @@
-import "https://appsforoffice.microsoft.com/lib/1/custom-functions-runtime.js";
+/* global CustomFunctions */
+
+// Load the Custom Functions runtime
+importScripts(
+  "https://appsforoffice.microsoft.com/lib/1/custom-functions-runtime.js"
+);
 
 function getrandom(x) {
-  if (x == "phone") {
-    return "54321";
-  } else if (x == "zip") {
+  if (x === "phone") {
+    return "5432167890";
+  } else if (x === "zip") {
     return "12345";
-  } else {
-    return "not supported";
   }
+  return "not supported";
 }
+
 CustomFunctions.associate("RANDOM", getrandom);
